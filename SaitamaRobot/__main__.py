4b7815480cdc51ecddc98f1a8864ef9457e9899b
LEGENDARY KEYS <70282108+Keys-007@users.noreply.github.com>
2021-03-12 11:43:22 +0530

    Sed

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -52,14 +52,13 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 Hi {}, my name is {}! 
-I am an 🚀Anime themed group management bot.
-Build by weebs for weebs, I specialize in managing anime and similar themed groups.
+I am an Modular group management bot.
 You can find my list of available commands with /help.
 """
 
 HELP_STRINGS = """
-Hey there! 👸My name is *{}*.
-I'm Dr Stone & I help admins to manage their groups! Have a look at the following for an idea of some of \
+Hey there!My name is *{}*.
+I'm Tedeza & I help admins to manage their groups! Have a look at the following for an idea of some of \
 the things I can help you with.
 
 *Main* commands available:
@@ -77,12 +76,10 @@ And the following:
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-SAITAMA_IMG = "https://telegra.ph/file/dc23351053dbb093f5076.jpg"
+SAITAMA_IMG = "https://telegra.ph/file/93300326241ed6b4d022c.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-Senku is hosted on one of Heroku's Servers and doesn't require any donations as of now but \
-You can donate to the original writer of the Base code, Paul
-There are two ways of supporting him; [PayPal](paypal.me/Kidiloska TG), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+Contact @Unbeatablex """
 
 IMPORTED = {}
 MIGRATEABLE = []
@@ -200,26 +197,26 @@ def start(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(
                     [[
                         InlineKeyboardButton(
-                            text="🚀ADD ME🏓",
+                            text="ADD ME",
                             url="t.me/{}?startgroup=true".format(
                                 context.bot.username)),
                          InlineKeyboardButton(
-                             text=" 🔔 Updates ",
-                             url="https://t.me/TG_BotZ")
+                             text=" Updates & Support ",
+                             url="https://t.me/Tedeza_support")
                      ],
                      [
                         InlineKeyboardButton(
-                            text=" 🔮Help ",
+                            text=" Help ",
                             url="https://t.me/ELIZABETHTG_bot?start=help"),
                          InlineKeyboardButton(
-                            text=" 🚴Made by 🛸 ",
-                             url="https://t.me/rosebakthan")        
+                            text=" Made by  ",
+                             url="https://t.me/Unbeatablex")        
                        
                      ],
                      [
                         InlineKeyboardButton(
-                             text=" 🔖 Source Code ",
-                             url="https://t.me/joinchat/WCAe3d8Ou71LTErh")
+                             text=" Source Code ",
+                             url="https://t.me/Unbeatablex")
                     
                     ]]))
     else:
@@ -323,7 +320,7 @@ def get_help(update: Update, context: CallbackContext):
         if len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
             module = args[1].lower()
             update.effective_message.reply_text(
-                f"Contact me in PM to get 🔖help of {module.capitalize()}",
+                f"Contact me in PM to get help of {module.capitalize()}",
                 reply_markup=InlineKeyboardMarkup([[
                     InlineKeyboardButton(
                         text="Help",
